2f8eaed6400cf4e6dda74a554dfbf0f1d2dbcdc0
Koby Johnson <knj11@vt.edu>
2021-04-18 14:21:50 -0400

    Completing the moving Zeros to the end challenge

---
+++ movingZerosToTheEnd.js
@@ -0,0 +1,12 @@
+/*Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+ test case: moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]*/
+
+var moveZeros = function (arr) {
+  let zeroArray = arr.filter((el) => (el === 0) ? true : false)
+  let nonZeroArray = arr.filter((el) => (el !== 0) ? true : false)
+
+  return [...nonZeroArray, ...zeroArray]
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]))
+
